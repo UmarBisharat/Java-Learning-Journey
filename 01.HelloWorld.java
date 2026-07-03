c75ce611aee73776eb1b6c9c303133244ff16294
umarbisharat303 <umarbisharat303@email.com>
2026-07-03 06:46:14 +0500

    updated

--- 01.HelloWorld.java
+++ 01.HelloWorld.java
@@ -6,6 +6,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         // Print the message "Hello, World!" to the console
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Umar!");
     }
 }
